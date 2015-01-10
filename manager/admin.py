e7d62ccfbe6263cd252c13b46246ec2be14ea04d
Dejan Levec <dejan@dejanlevec.com>
2015-01-10 16:21:23 +0100

    auto set added_by field

--- manager/admin.py
+++ manager/admin.py
@@ -9,16 +9,11 @@ class MyModelAdmin(admin.ModelAdmin):
             'js/admin.js',
         )
 
-class AccountAdmin(MyModelAdmin):
-    change_list_template = 'admin/accounts.html'
-
-
-admin.site.register(Account, AccountAdmin)
-
 class AppAdmin(MyModelAdmin):
     change_list_template = 'admin/apps.html'
     list_display = ['name', 'account', 'image', 'container_id', 'memory', 'status', 'start', 'stop', 'redeploy']
     list_filter = ('account', 'memory', 'image', )
+    readonly_fields = ('added_by',)
 
     def status(self, form):
         return ''
@@ -38,18 +33,41 @@ class AppAdmin(MyModelAdmin):
 
     redeploy.allow_tags = True
 
+    def save_model(self, request, obj, form, change):
+        try:
+            obj.added_by
+        except:
+            obj.added_by = request.user
+        obj.save()
+
 admin.site.register(App, AppAdmin)
 
 
 class DatabaseAdmin(MyModelAdmin):
     change_list_template = 'admin/databases.html'
     list_display = ['name', 'account']
+    readonly_fields = ('added_by',)
+
+    def save_model(self, request, obj, form, change):
+        try:
+            obj.added_by
+        except:
+            obj.added_by = request.user
+        obj.save()
 
 admin.site.register(Database, DatabaseAdmin)
 
 class DomainAdmin(MyModelAdmin):
     list_display = ['name', 'account', 'redirect_url', 'apache_enabled']
+    readonly_fields = ('added_by',)
     change_list_template = 'admin/domains.html'
+
+    def save_model(self, request, obj, form, change):
+        try:
+            obj.added_by
+        except:
+            obj.added_by = request.user
+        obj.save()
 
 admin.site.register(Domain, DomainAdmin)
 
@@ -62,5 +80,27 @@ class ImageVariableInline(admin.StackedInline):
 
 class ImageAdmin(MyModelAdmin):
     inlines = (ImagePortInline, ImageVariableInline, )
+    readonly_fields = ('added_by',)
+
+    def save_model(self, request, obj, form, change):
+        try:
+            obj.added_by
+        except:
+            obj.added_by = request.user
+        obj.save()
 
 admin.site.register(Image, ImageAdmin)
+
+class AccountAdmin(MyModelAdmin):
+    change_list_template = 'admin/accounts.html'
+    readonly_fields = ('added_by',)
+
+    def save_model(self, request, obj, form, change):
+        try:
+            obj.added_by
+        except:
+            obj.added_by = request.user
+        obj.save()
+
+
+admin.site.register(Account, AccountAdmin)
