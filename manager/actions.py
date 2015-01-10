15be82ff00d0cd8cdf4122a8014a717edb538052
Dejan Levec <dejan@dejanlevec.com>
2015-01-10 11:29:01 +0100

    apache2

--- manager/actions.py
+++ manager/actions.py
@@ -7,6 +7,7 @@ from manager import docker_api
 from manager.models import Account, Domain, Database
 from manager.utils import exec_command, Logs
 import os
+import re
 
 
 def sync_accounts():
@@ -30,6 +31,7 @@ def sync_accounts():
         ]
 
         o, e = exec_command(logs, "sudo adduser nginx %s" % account.name)
+        o, e = exec_command(logs, "sudo adduser apache %s" % account.name)
 
         for dir in dirs:
             o, e = exec_command(logs, "sudo mkdir -p /home/%s/%s" % (account.name, dir))
@@ -92,6 +94,7 @@ def update_nginx_config():
 
     #remove existing files
     o, e = exec_command(logs, "sudo rm /etc/nginx/manager/*")
+    o, e = exec_command(logs, "sudo rm /etc/apache2/manager/*")
 
     for account in Account.objects.all():
         domains = account.domains.all()
@@ -114,6 +117,7 @@ def update_nginx_config():
             logs.add("domain %s, account %s" % (domain.name, domain.account.name))
 
             nginx = ''
+            apache = domain.apache_config
 
             if domain.redirect_url:
                 #redirect to url
@@ -127,6 +131,11 @@ def update_nginx_config():
 
                 for port in port_mapping:
                     nginx = nginx.replace('#%s#' % port, port_mapping[port])
+
+                    if domain.apache_enabled:
+                        apache = apache.replace('#%s#' % port, port_mapping[port])
+
+                nginx = nginx.replace('#apache#', '127.0.0.1:8080')
 
             logs.add(nginx)
 
@@ -147,10 +156,44 @@ def update_nginx_config():
                 logs.add('NGINX CONFIG TEST FAILED!')
                 return logs
 
+
+            #apache
+            if domain.apache_enabled:
+                conf = render_to_string('system/apache_vhost.conf', {
+                    'domain': domain.name,
+                    'config': apache
+                })
+
+                #find <php></php>
+                m = re.search('<php>(.*)<\/php>', conf)
+                for x in m.groups():
+                    name = domain.name.replace('.', '-')
+                    txt = """AddType application/x-httpd-fastphp5 .php
+            Action application/x-httpd-fastphp5 /php5-fcgi
+            Alias /php5-fcgi /usr/lib/cgi-bin/php5-fcgi-%s
+            FastCgiExternalServer /usr/lib/cgi-bin/php5-fcgi-%s -host %s:9000 -pass-header Authorization""" % (name, name, x)
+                    conf = conf.replace('<php>%s</php>' % x, txt)
+
+                logs.add(conf)
+
+                fname = os.path.join('/etc/apache2/manager', '%s_%s.conf' % (account.name, domain.name))
+
+                with open(fname, 'w') as f:
+                    f.write(conf)
+
+                #test config
+                o, e = exec_command(logs, "sudo apachectl configtest")
+
+            if 'failed' in o:
+                logs.add('APACHE CONFIG TEST FAILED!')
+                return logs
+
+
     #reload nginx
     o, e = exec_command(logs, "sudo service nginx reload")
 
-
+    #reload apache2
+    o, e = exec_command(logs, "sudo service apache2 reload")
 
     return logs
 
