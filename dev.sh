58b271c954f1d7b211fbcad13c20ef5b3c4af455
Dejan Levec <dejan@dejanlevec.com>
2015-03-03 22:00:45 +0100

    dev scripts cleanup

---
+++ dev.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+#!/bin/bash
+
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+cd $DIR
+
+export GOROOT=/usr/local/go1.4/go
+export GOPATH=$DIR/go-libs/
+export GOBIN=$DIR/go-libs/bin/
+
+export LC_CTYPE=en_US.UTF-8
+export LC_ALL=en_US.UTF-8
+
+if [ $1 = "cron" ]; then
+	echo "cron"
+
+	cd ./cron
+	/usr/local/go1.4/go/bin/go get
+	/usr/local/go1.4/go/bin/go install
+	/usr/local/go1.4/go/bin/go run main.go
+elif [ $1 = "proxy" ]; then
+	echo "proxy"
+
+	cd ./proxy
+	/usr/local/go1.4/go/bin/go get
+	/usr/local/go1.4/go/bin/go install
+	/usr/local/go1.4/go/bin/go run main.go session.go shell.go
+elif [ $1 = "ondemand" ]; then
+	echo "ondemand"
+
+	cd ./ondemand
+		/usr/local/go1.4/go/bin/go get
+	/usr/local/go1.4/go/bin/go install
+	/usr/local/go1.4/go/bin/go run main.go
+elif [ $1 = "manager" ]; then
+	echo "manager"
+
+	source env/bin/activate
+	python manage.py update-nginx-config --settings=manager.settings.dev
+	python manage.py runserver 0.0.0.0:5555 --settings=manager.settings.dev
+else
+	echo "wrong argument"
+fi
