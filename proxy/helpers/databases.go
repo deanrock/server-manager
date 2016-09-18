c21eab648d0b7686dd872bd89dbe11f2075870be
xTrinch <mojca.rojko@gmail.com>
2016-09-18 18:44:21 +0200

    Revoke all privileges to public

--- proxy/helpers/databases.go
+++ proxy/helpers/databases.go
@@ -1,81 +1,81 @@
 package helpers
 
 import (
-  "../models"
-  "database/sql"
-  _ "github.com/go-sql-driver/mysql"
-  _ "github.com/lib/pq"
-  "fmt"
+	"../models"
+	"database/sql"
+	"fmt"
+	_ "github.com/go-sql-driver/mysql"
+	_ "github.com/lib/pq"
 )
 
 func CreateMysqlDatabase(database *models.Database) (bool, error) {
 
-  success := true
-  db, err := sql.Open("mysql", "root:"+"password"+"@tcp(127.0.0.1:3306)/")
-  defer db.Close()
+	success := true
+	db, err := sql.Open("mysql", "root:"+"password"+"@tcp(127.0.0.1:3306)/")
+	defer db.Close()
 
-  if err != nil {
-    fmt.Println(err)
-    return false, err
-  }
+	if err != nil {
+		fmt.Println(err)
+		return false, err
+	}
 
-  _, err = db.Exec("CREATE DATABASE `"+database.Name+"` CHARACTER SET utf8 COLLATE utf8_general_ci")
+	_, err = db.Exec("CREATE DATABASE `" + database.Name + "` CHARACTER SET utf8 COLLATE utf8_general_ci")
 
-  if err != nil {
-    return false, err
-  }
+	if err != nil {
+		return false, err
+	}
 
-  _, err = db.Exec("GRANT ALL ON `"+database.Name+"`.* TO `"+database.User+"`@'%%' IDENTIFIED BY '"+database.Password+"'")
+	_, err = db.Exec("GRANT ALL ON `" + database.Name + "`.* TO `" + database.User + "`@'%%' IDENTIFIED BY '" + database.Password + "'")
 
-  if err != nil {
-    return false, err
-  }
+	if err != nil {
+		return false, err
+	}
 
-  _, err = db.Exec("FLUSH PRIVILEGES")
+	_, err = db.Exec("FLUSH PRIVILEGES")
 
-  return success, err
+	return success, err
 }
 
 func CreatePostgresDatabase(database *models.Database) (bool, error) {
 
-  success := true
-  db, err := sql.Open("postgres", "user=postgres password=password sslmode=disable host=localhost")
-  defer db.Close()
+	success := true
+	db, err := sql.Open("postgres", "user=postgres password=password sslmode=disable host=localhost")
+	defer db.Close()
 
-  if err != nil {
-    fmt.Println(err)
-    return false, err
-  }
+	if err != nil {
+		fmt.Println(err)
+		return false, err
+	}
 
-  _, err = db.Exec("CREATE DATABASE "+database.Name+" ENCODING 'utf8' LC_COLLATE 'en_US.UTF-8'")
+	_, err = db.Exec("CREATE DATABASE " + database.Name + " ENCODING 'utf8' LC_COLLATE 'en_US.UTF-8'")
 
-  if err != nil {
-    fmt.Println(err)
-    return false, err
-  }
+	if err != nil {
+		fmt.Println(err)
+		return false, err
+	}
 
-  _, err = db.Exec("CREATE ROLE " + database.User + " WITH LOGIN PASSWORD '" + database.Password + "'")
+	_, err = db.Exec("CREATE ROLE " + database.User + " WITH LOGIN PASSWORD '" + database.Password + "'")
 
-  if err != nil {
-    fmt.Println(err)
-    return false, err
-  }
+	if err != nil {
+		fmt.Println(err)
+		return false, err
+	}
 
-  _, err = db.Exec("REVOKE connect ON DATABASE "+database.Name+" FROM PUBLIC")
+	_, err = db.Exec("REVOKE ALL PRIVILEGES ON DATABASE " + database.Name + " FROM PUBLIC")
 
-  if err != nil {
-    fmt.Println(err)
-    return false, err
-  }
+	if err != nil {
+		fmt.Println(err)
+		return false, err
+	}
 
-  _, err = db.Exec("GRANT connect ON DATABASE "+database.Name+" TO "+database.User)
+	_, err = db.Exec("GRANT ALL PRIVILEGES ON DATABASE " + database.Name + " TO " + database.User)
 
-  if err != nil {
-    fmt.Println(err)
-    return false, err
-  }
+	if err != nil {
+		fmt.Println(err)
+		return false, err
+	}
 
-  _, err = db.Exec("FLUSH PRIVILEGES")
+	_, err = db.Exec("FLUSH PRIVILEGES")
 
-  return success, err
+	return success, err
 }
